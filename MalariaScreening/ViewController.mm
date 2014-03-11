3608c8672085649d4247ac1f92865964d7c2c003
bidgevelvet <decha_big@hotmail.com>
2014-03-11 14:27:49 +0700

    change to use OpenCV — Big (+ OpenCV Library)

---
+++ MalariaScreening/ViewController.mm
@@ -0,0 +1,346 @@
+//
+//  ViewController.m
+//  MalariaScreening
+//
+//  Created by Decha Tesapirat on 2/12/2557 BE.
+//  Copyright (c) 2557 Decha Tesapirat. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIImageCVMatConverter.h"
+@interface ViewController ()
+@property (nonatomic, weak) IBOutlet UIImageView *imageView;
+
+@property (nonatomic, weak) IBOutlet UIToolbar *toolBar;
+
+@property (nonatomic) IBOutlet UIView *overlayView;
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *takePictureButton;
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *startStopButton;
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *delayedPhotoButton;
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *doneButton;
+
+@property (nonatomic) UIImagePickerController *imagePickerController;
+
+@property (nonatomic, weak) NSTimer *cameraTimer;
+@property (nonatomic) NSMutableArray *capturedImages;
+
+@property UIImage *finalImage;
+
+@end
+
+@implementation ViewController
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.capturedImages = [[NSMutableArray alloc] init];
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        // There is not a camera on this device, so don't show the camera button.
+        NSMutableArray *toolbarItems = [self.toolBar.items mutableCopy];
+        [toolbarItems removeObjectAtIndex:2];
+        [self.toolBar setItems:toolbarItems animated:NO];
+    }
+}
+
+
+- (IBAction)showImagePickerForCamera:(id)sender
+{
+    [self showImagePickerForSourceType:UIImagePickerControllerSourceTypeCamera];
+}
+
+
+- (IBAction)showImagePickerForPhotoPicker:(id)sender
+{
+    [self showImagePickerForSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+}
+
+
+- (void)showImagePickerForSourceType:(UIImagePickerControllerSourceType)sourceType
+{
+    if (self.imageView.isAnimating)
+    {
+        [self.imageView stopAnimating];
+    }
+    
+    if (self.capturedImages.count > 0)
+    {
+        [self.capturedImages removeAllObjects];
+    }
+    
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    imagePickerController.sourceType = sourceType;
+    imagePickerController.delegate = self;
+    
+    if (sourceType == UIImagePickerControllerSourceTypeCamera)
+    {
+        /*
+         The user wants to use the camera interface. Set up our custom overlay view for the camera.
+         */
+        imagePickerController.showsCameraControls = NO;
+        
+        /*
+         Load the overlay view from the OverlayView nib file. Self is the File's Owner for the nib file, so the overlayView outlet is set to the main view in the nib. Pass that view to the image picker controller to use as its overlay view, and set self's reference to the view to nil.
+         */
+        [[NSBundle mainBundle] loadNibNamed:@"OverlayView" owner:self options:nil];
+        self.overlayView.frame = imagePickerController.cameraOverlayView.frame;
+        imagePickerController.cameraOverlayView = self.overlayView;
+        self.overlayView = nil;
+    }
+    
+    self.imagePickerController = imagePickerController;
+    [self presentViewController:self.imagePickerController animated:YES completion:nil];
+}
+
+
+#pragma mark - Toolbar actions
+
+- (IBAction)done:(id)sender
+{
+    // Dismiss the camera.
+    if ([self.cameraTimer isValid])
+    {
+        [self.cameraTimer invalidate];
+    }
+    [self finishAndUpdate];
+}
+
+
+- (IBAction)takePhoto:(id)sender
+{
+    [self.imagePickerController takePicture];
+}
+
+
+- (IBAction)delayedTakePhoto:(id)sender
+{
+    // These controls can't be used until the photo has been taken
+    self.doneButton.enabled = NO;
+    self.takePictureButton.enabled = NO;
+    self.delayedPhotoButton.enabled = NO;
+    self.startStopButton.enabled = NO;
+    
+    NSDate *fireDate = [NSDate dateWithTimeIntervalSinceNow:5.0];
+    NSTimer *cameraTimer = [[NSTimer alloc] initWithFireDate:fireDate interval:1.0 target:self selector:@selector(timedPhotoFire:) userInfo:nil repeats:NO];
+    
+    [[NSRunLoop mainRunLoop] addTimer:cameraTimer forMode:NSDefaultRunLoopMode];
+    self.cameraTimer = cameraTimer;
+}
+
+
+- (IBAction)startTakingPicturesAtIntervals:(id)sender
+{
+    /*
+     Start the timer to take a photo every 1.5 seconds.
+     
+     CAUTION: for the purpose of this sample, we will continue to take pictures indefinitely.
+     Be aware we will run out of memory quickly.  You must decide the proper threshold number of photos allowed to take from the camera.
+     One solution to avoid memory constraints is to save each taken photo to disk rather than keeping all of them in memory.
+     In low memory situations sometimes our "didReceiveMemoryWarning" method will be called in which case we can recover some memory and keep the app running.
+     */
+    self.startStopButton.title = NSLocalizedString(@"Stop", @"Title for overlay view controller start/stop button");
+    [self.startStopButton setAction:@selector(stopTakingPicturesAtIntervals:)];
+    
+    self.doneButton.enabled = NO;
+    self.delayedPhotoButton.enabled = NO;
+    self.takePictureButton.enabled = NO;
+    
+    self.cameraTimer = [NSTimer scheduledTimerWithTimeInterval:1.5 target:self selector:@selector(timedPhotoFire:) userInfo:nil repeats:YES];
+    [self.cameraTimer fire]; // Start taking pictures right away.
+}
+
+
+- (IBAction)stopTakingPicturesAtIntervals:(id)sender
+{
+    // Stop and reset the timer.
+    [self.cameraTimer invalidate];
+    self.cameraTimer = nil;
+    
+    [self finishAndUpdate];
+}
+
+
+- (void)finishAndUpdate
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+    
+    if ([self.capturedImages count] > 0)
+    {
+        if ([self.capturedImages count] == 1)
+        {
+            // Camera took a single picture.
+            [self.imageView setImage:[self.capturedImages objectAtIndex:0]];
+        }
+        else
+        {
+            // Camera took multiple pictures; use the list of images for animation.
+            self.imageView.animationImages = self.capturedImages;
+            self.imageView.animationDuration = 5.0;    // Show each captured photo for 5 seconds.
+            self.imageView.animationRepeatCount = 0;   // Animate forever (show all photos).
+            [self.imageView startAnimating];
+        }
+        
+        // To be ready to start again, clear the captured images array.
+        self.finalImage = [self.capturedImages objectAtIndex:0];
+        [self.capturedImages removeAllObjects];
+    }
+    
+    self.imagePickerController = nil;
+}
+
+
+#pragma mark - Timer
+
+// Called by the timer to take a picture.
+- (void)timedPhotoFire:(NSTimer *)timer
+{
+    [self.imagePickerController takePicture];
+}
+
+
+#pragma mark - UIImagePickerControllerDelegate
+
+// This method is called when an image has been chosen from the library or taken from the camera.
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    
+    [self.capturedImages addObject:image];
+    
+    if ([self.cameraTimer isValid])
+    {
+        return;
+    }
+    
+    [self finishAndUpdate];
+}
+
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+-(UIColor*)pixelAtXY:(NSInteger)pointX and:(NSInteger)pointY
+{
+    CGImageRef cgImage = self.finalImage.CGImage;
+    NSUInteger width = CGImageGetWidth(cgImage);
+    NSUInteger height = CGImageGetHeight(cgImage);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    int bytesPerPixel = 4;
+    int bytesPerRow = bytesPerPixel * 1;
+    NSUInteger bitsPerComponent = 8;
+    unsigned char pixelData[4] = { 0, 0, 0, 0 };
+    CGContextRef context = CGBitmapContextCreate(pixelData,
+                                                 1,
+                                                 1,
+                                                 bitsPerComponent,
+                                                 bytesPerRow,
+                                                 colorSpace,
+                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+    CGContextSetBlendMode(context, kCGBlendModeCopy);
+    
+    // Draw the pixel we are interested in onto the bitmap context
+    CGContextTranslateCTM(context, -pointX, -pointY);
+    CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, (CGFloat)width, (CGFloat)height), cgImage);
+    CGContextRelease(context);
+    
+    // Convert color values [0..255] to floats [0.0..1.0]
+    CGFloat red   = (CGFloat)pixelData[0] / 255.0f;
+    CGFloat green = (CGFloat)pixelData[1] / 255.0f;
+    CGFloat blue  = (CGFloat)pixelData[2] / 255.0f;
+    CGFloat alpha = (CGFloat)pixelData[3] / 255.0f;
+    NSLog(@"red = %f",red);
+    NSLog(@"green = %f",green);
+    NSLog(@"blue = %f",blue);
+    //create and return UIColor
+    UIColor *acolor = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+    return acolor;
+}
+- (void)changeLabel:(id)sender{
+    [self greyScaleImage:self.finalImage];
+   // UIColor *acolor = [self pixelAtXY:1 and:2];
+    //change label.text
+//    CGFloat red;
+//    CGFloat green;
+//    CGFloat blue;
+//    CGFloat alpha;
+//    NSArray *tester;
+//    [acolor getRed:&red green:&green blue:&blue alpha:&alpha];
+//    
+//    showRed.text = [NSString stringWithFormat: @"%.2f", red];
+//    showGreen.text = [NSString stringWithFormat: @"%.2f", green];
+//    showBlue.text = [NSString stringWithFormat: @"%.2f", blue];
+//    NSInteger noob  = self.finalImage.size.width*100;
+//   for(int i=0;i<self.finalImage.size.height;i++){
+//    tester = [self getRGBAsFromImage:self.finalImage atX:0 andY:i*100 count:noob];
+//   }
+
+}
+//- (NSArray*)arrayOfImageColor:(id)sender{
+//    
+//    NSMutableArray *colorArray = [[NSMutableArray alloc]init];
+//    for (int i = 0;i<self.finalImage.size.height;i++) {
+//        NSLog(@"For i = %i",i);
+//        for(int j= 0;j<self.finalImage.size.width;j++){
+//            NSLog(@"for j = %i",j);
+//            UIColor *color = [self pixelAtXY:i and:j];
+//            [colorArray addObject:color];
+//        }
+//    }
+//
+//    return colorArray;
+//    
+//    
+//}
+- (NSArray*)getRGBAsFromImage:(UIImage*)image atX:(int)xx andY:(int)yy count:(int)count
+{
+    NSMutableArray *result = [NSMutableArray arrayWithCapacity:count];
+    
+    // First get the image into your data buffer
+    CGImageRef imageRef = [image CGImage];
+    NSUInteger width = CGImageGetWidth(imageRef);
+    NSUInteger height = CGImageGetHeight(imageRef);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char *rawData = (unsigned char*) calloc(height * width * 4, sizeof(unsigned char));
+    NSUInteger bytesPerPixel = 4;
+    NSUInteger bytesPerRow = bytesPerPixel * width;
+    NSUInteger bitsPerComponent = 8;
+    CGContextRef context = CGBitmapContextCreate(rawData, width, height,
+                                                 bitsPerComponent, bytesPerRow, colorSpace,
+                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
+    CGContextRelease(context);
+    
+    // Now your rawData contains the image data in the RGBA8888 pixel format.
+    int byteIndex = (bytesPerRow * yy) + xx * bytesPerPixel;
+    for (int ii = 0 ; ii < count ; ++ii)
+    {
+        CGFloat red   = (rawData[byteIndex]     * 1.0) / 255.0;
+        CGFloat green = (rawData[byteIndex + 1] * 1.0) / 255.0;
+        CGFloat blue  = (rawData[byteIndex + 2] * 1.0) / 255.0;
+        CGFloat alpha = (rawData[byteIndex + 3] * 1.0) / 255.0;
+        byteIndex += 4;
+        NSLog(@"pixel count %i",ii);
+        UIColor *acolor = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+        [result addObject:acolor];
+    }
+    
+    free(rawData);
+    NSLog(@"%@",result);
+    return result;
+}
+-(void)greyScaleImage:(UIImage*)image
+{
+    cv::Mat mat = [UIImageCVMatConverter cvMatFromUIImage:image];
+    cv::Mat matOut;
+    cv::cvtColor(mat, matOut, CV_RGB2GRAY);
+    self.finalImage = [UIImageCVMatConverter UIImageFromCVMat:matOut];
+    [self.imageView setImage:self.finalImage];
+}
+
+@end
